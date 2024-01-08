fc3e82651db5979f3ac3559e77b5e59babefebcf
jinsu4755 <parkjinsu4755@gmail.com>
2024-01-09 02:38:39 +0900

    feat: 물건 이름 관련 로직 수정
    물건 이름에 관련된 부분은 도메인이 아닌 뷰에서 보여지는 로직이므로 이를 변경함

--- app/src/main/java/org/care/packie/ui/component/stuff/StuffLabel.kt
+++ app/src/main/java/org/care/packie/ui/component/stuff/StuffLabel.kt
@@ -5,19 +5,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import org.care.packie.domain.StuffName
 import org.care.packie.ui.theme.PackieDesignSystem
 
 @Composable
 fun StuffLabel(
     label: String
 ) = Text(
-    text = StuffName(label).limitMaxLength(StuffName.LIMITE_LENGHT),
+    text = StuffName(label).limitMaxLength(StuffName.LIMITED_LENGTH),
     style = PackieDesignSystem.typography.content,
     color = PackieDesignSystem.colors.white,
     maxLines = 1,
     overflow = TextOverflow.Ellipsis
 )
+
+data class StuffName(val name: String) {
+    fun limitMaxLength(limit: Int, overflowToken: String = OVERFLOW_ELLIPSIS): String {
+        return if (name.length > limit) {
+            "${name.substring(0 until limit)}${overflowToken}"
+        } else {
+            name
+        }
+    }
+
+    companion object {
+        private const val OVERFLOW_ELLIPSIS = "⋯"
+        const val LIMITED_LENGTH = 7
+    }
+}
 
 @Preview(widthDp = 159)
 @Composable
