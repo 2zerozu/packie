26a9de5d07c44bdf67db37aebc1cdeff8f126d8b
jinsu4755 <parkjinsu4755@gmail.com>
2024-01-08 23:31:41 +0900

    feat: 물건 이름 도메인 VO 작성

---
+++ app/src/main/java/org/care/packie/domain/StuffItemName.kt
@@ -0,0 +1,16 @@
+package org.care.packie.domain
+
+data class StuffItemName(private val name: String) {
+    fun limitMaxLength(limit: Int, overflowToken: String = OVERFLOW_ELLIPSIS): String {
+        return if (name.length > limit) {
+            "${name.substring(0 until limit)}${overflowToken}"
+        } else {
+            name
+        }
+    }
+
+    companion object {
+        private const val OVERFLOW_ELLIPSIS = "⋯"
+        const val LIMITE_LENGHT = 7
+    }
+}
