4be48b71f7ef54d441f5ad840a42a779d5bcfbf8
이빵주 <yungju9982@naver.com>
2024-01-09 02:49:19 +0900

    feat: animation

--- app/src/main/java/org/care/packie/feature/packingCategory/PackingScreen.kt
+++ app/src/main/java/org/care/packie/feature/packingCategory/PackingScreen.kt
@@ -1,10 +1,12 @@
 package org.care.packie.feature.packingCategory
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +15,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.onebone.toolbar.CollapsingToolbarScaffold
+import me.onebone.toolbar.ScrollStrategy
+import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
 import org.care.packie.R
+import org.care.packie.ui.component.common.PackieButton
 import org.care.packie.ui.component.packingCategory.PackingCategory
 import org.care.packie.ui.theme.PackieDesignSystem
 import org.care.packie.ui.theme.PackieTheme
@@ -22,33 +29,58 @@ import org.care.packie.ui.theme.PackieTheme
 fun PackingScreen(
     categories: List<String>
 ) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(42.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(modifier = Modifier.size(60.dp))
+    val state = rememberCollapsingToolbarScaffoldState()
 
-        Text(
-            text = stringResource(id = R.string.packing_category_title),
-            style = PackieDesignSystem.typography.title,
-            color = PackieDesignSystem.colors.white,
-        )
+    Column {
+        CollapsingToolbarScaffold(
+            modifier = Modifier.fillMaxSize(),
+            state = state,
+            scrollStrategy = ScrollStrategy.ExitUntilCollapsed,
+            toolbar = {
+                val fontSize = (15 + (20 - 15) * state.toolbarState.progress).sp
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(130.dp)
+                        .pin()
+                )
 
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier.padding(horizontal = 16.dp)
+                Text(
+                    text = stringResource(id = R.string.packing_category_title),
+                    style = PackieDesignSystem.typography.title,
+                    color = PackieDesignSystem.colors.white,
+                    modifier = Modifier.road(Alignment.TopCenter, Alignment.Center),
+                    fontSize = fontSize
+                )
+
+            }
         ) {
-            items(categories.size) { index ->
-                PackingCategory(category = categories[index])
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                items(categories.size) { index ->
+                    PackingCategory(category = categories[index])
+                }
             }
         }
+        PackieButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = stringResource(id = R.string.packing_category_button))
+        }
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PackingScreenPreview() {
     PackieTheme {
-        PackingScreen(listOf("출근", "놀러갈 때", "여행"))
+        PackingScreen(listOf("출근", "놀러갈 때", "여행", "출근", "놀러갈 때", "여행", "출근", "놀러갈 때", "여행"))
     }
 }
