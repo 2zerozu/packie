f784794627503c2e15232d88a6580135fad38eef
이빵주 <yungju9982@naver.com>
2024-01-09 07:38:50 +0900

    feat: dialog position up

--- app/src/main/java/org/care/packie/feature/packingCategory/PackingScreen.kt
+++ app/src/main/java/org/care/packie/feature/packingCategory/PackingScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -111,19 +112,21 @@ fun PackingScreen(
             } else Modifier),
         contentAlignment = Alignment.Center
     ) {
-        AnimatedVisibility(
-            visible = isDialogOpen,
-            enter = fadeIn(),
-            exit = fadeOut(),
-        ) {
-            AddDialog(
-                type = AddDialogType.PACKING_CATEGORY,
-                onConfirmation = {
-                    Log.d("asdf", "\'$it\'이 추가되었습니다")
-                    isDialogOpen = false
-                },
-                onDismiss = { isDialogOpen = false }
-            )
+        Box(modifier = Modifier.offset(y = (-60).dp)) {
+            AnimatedVisibility(
+                visible = isDialogOpen,
+                enter = fadeIn(),
+                exit = fadeOut(),
+            ) {
+                AddDialog(
+                    type = AddDialogType.PACKING_CATEGORY,
+                    onConfirmation = {
+                        Log.d("asdf", "\'$it\'이 추가되었습니다")
+                        isDialogOpen = false
+                    },
+                    onDismiss = { isDialogOpen = false }
+                )
+            }
         }
     }
 }
