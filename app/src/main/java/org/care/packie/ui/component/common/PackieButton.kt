80fd34c3bb1506fff2f8b3b0b1619a428ebecc28
jinsu4755 <parkjinsu4755@gmail.com>
2024-01-08 22:26:26 +0900

    feat: Packie 테마 버튼 추가

---
+++ app/src/main/java/org/care/packie/ui/component/common/PackieButton.kt
@@ -0,0 +1,46 @@
+package org.care.packie.ui.component.common
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.care.packie.ui.theme.PackieDesignSystem
+
+@Composable
+fun PackieButton(
+    modifier: Modifier = Modifier,
+    buttonColors: ButtonColors = ButtonDefaults.buttonColors(
+        containerColor = PackieDesignSystem.colors.buttonWhite,
+        contentColor = PackieDesignSystem.colors.black
+    ),
+    shape: Shape = RoundedCornerShape(8.dp),
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit
+) = Button(
+    modifier = modifier,
+    colors = buttonColors,
+    shape = shape,
+    onClick = onClick
+) {
+    ProvideTextStyle(value = PackieDesignSystem.typography.subTitle) {
+        content()
+    }
+}
+
+@Preview
+@Composable
+fun PackieButtonPreview() {
+    PackieButton(
+        onClick = {}
+    ) {
+        Text(text = "hello")
+    }
+}
