c3f05251e2d2df296da6fb191d713d35d6b9e87b
jinsu4755 <parkjinsu4755@gmail.com>
2024-01-11 01:23:17 +0900

    feat: Text includeFontPadding 제거

--- app/src/main/java/org/care/packie/ui/theme/Type.kt
+++ app/src/main/java/org/care/packie/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package org.care.packie.ui.theme
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -20,21 +21,25 @@ fun packieTypography() = PackieTypography(
     title = TextStyle(
         fontFamily = NotoSansKrMedium,
         fontSize = 20.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = false),
         fontWeight = FontWeight.Normal
     ),
     subTitle = TextStyle(
         fontFamily = NotoSansKrMedium,
         fontSize = 16.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = false),
         fontWeight = FontWeight.Normal
     ),
     content = TextStyle(
         fontFamily = NotoSansKrMedium,
         fontSize = 14.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = false),
         fontWeight = FontWeight.Normal
     ),
     body = TextStyle(
         fontFamily = NotoSansKrMedium,
         fontSize = 12.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = false),
         fontWeight = FontWeight.Normal
     )
 )
