8d641a38319347f65dea2daffcc72e2b572ca2bb
jinsu4755 <parkjinsu4755@gmail.com>
2024-01-20 05:33:57 +0900

    feat: 네비게이션 라우터 설정

--- app/src/main/java/org/care/packie/MainActivity.kt
+++ app/src/main/java/org/care/packie/MainActivity.kt
@@ -3,7 +3,12 @@ package org.care.packie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.care.packie.feature.stuffs.StuffsScreenRoot
 import org.care.packie.ui.theme.PackieTheme
@@ -21,7 +26,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PackieApp() {
+    val navController = rememberNavController()
     PackieTheme {
+        NavHost(
+            navController = navController,
+            startDestination = "test"
+        ) {
+
+        }
         StuffsScreenRoot()
     }
+}
+
+sealed class PackieNavDestination(
+    val route: String
+) {
+    object CategoryScreen: PackieNavDestination(
+        route = "category"
+    )
+    object StuffsScreen: PackieNavDestination(
+        route = "stuffs"
+    )
 }
