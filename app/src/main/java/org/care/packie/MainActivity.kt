1593d3ed689527292dffb8300752079227750bbe
jinsu4755 <parkjinsu4755@gmail.com>
2024-01-28 18:36:39 +0900

    refactor: 불필요 import 제거

--- app/src/main/java/org/care/packie/MainActivity.kt
+++ app/src/main/java/org/care/packie/MainActivity.kt
@@ -3,19 +3,8 @@ package org.care.packie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import androidx.navigation.navigation
 import dagger.hilt.android.AndroidEntryPoint
 import org.care.packie.feature.PackieNavHost
-import org.care.packie.feature.category.CategoryScreenRoot
-import org.care.packie.feature.stuffs.StuffsScreenRoot
 import org.care.packie.ui.theme.PackieTheme
 
 @AndroidEntryPoint
--- app/src/main/java/org/care/packie/feature/PackieNavHost.kt
+++ app/src/main/java/org/care/packie/feature/PackieNavHost.kt
@@ -1,14 +1,8 @@
 package org.care.packie.feature
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navigation
-import org.care.packie.feature.category.CategoryScreenRoot
-import org.care.packie.feature.stuffs.StuffsScreenRoot
 
 @Composable
 fun PackieNavHost() {
