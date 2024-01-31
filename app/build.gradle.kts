a49835fdaef6efa03a46db8fccdd6ba322a49455
이빵주 <yungju9982@naver.com>
2024-02-01 02:50:03 +0900

    build: update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "org.care.packie"
         minSdk = 28
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
