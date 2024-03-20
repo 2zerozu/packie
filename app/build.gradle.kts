adb45ba2919c0b16f67f693c958f914d548abc13
jinsu4755 <parkjinsu4755@gmail.com>
2024-03-20 12:08:23 +0900

    build: ad_id 권한 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "org.care.packie"
         minSdk = 28
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.2"
+        versionCode = 7
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
